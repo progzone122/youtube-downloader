105e72f507814a6fe3438f24baddc19637aa22b5
Diablo <80642969+progzone122@users.noreply.github.com>
2022-04-07 17:28:35 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -30,10 +30,10 @@ def status(status):
 def download(url, quality):
     global config
     status('downloading')
-    path = config['output']['path']
+    path = config['output']['path'] + '/'
     my_video = YouTube(url).streams
-    my_video.filter(only_video = True, resolution=quality).first().download(path, '/v.mp4')
-    my_video.filter(only_audio=True).first().download(path, '/a.mp3')
+    my_video.filter(only_video = True, resolution=quality).first().download(path, 'v.mp4')
+    my_video.filter(only_audio=True).first().download(path, 'a.mp3')
     status('converting')
     cmd = "ffmpeg.exe -i " + path + "/v.mp4" + " -i " + path + "/a.mp3" + " -c:v copy -y " + path + "/output.mp4"
     os.system(cmd)
